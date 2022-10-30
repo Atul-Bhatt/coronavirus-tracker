f72bd6573af25f5d91acb699ec21434acfe9078a
Atul Bhatt <atulbhatt61@gmail.com>
2022-10-30 14:33:03 +0530

    first commit

---
+++ src/main/java/io/atulandjava/coronavirustracker/services/CoronaVirusDataService.java
@@ -0,0 +1,24 @@
+package io.atulandjava.coronavirustracker.services;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class CoronaVirusDataService {
+
+    private static String VIRUS_DATA_URL = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/01-01-2021.csv";
+
+    public void fetchVirusData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URL))
+                .build();
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(httpResponse.body());
+    }
+}
---
+++ src/test/java/io/atulandjava/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.atulandjava.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
